8260aa1180f85e1eeca378a450cf2a67065f1d33
josy <jvfariasm@gmail.com>
2021-10-19 22:47:48 -0300

    initial commit

---
+++ src/main/java/app/App.java
@@ -0,0 +1,10 @@
+package app;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//dbc:sqlite:C:\Users\josel\eclipse-workspace\conexion_db_agenciadeturismo\tierramedia.db
+	}
+
+}
