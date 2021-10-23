d7c33d5af2738cb85d19083b64eef558b70d011f
Josy <jvfariasm@gmail.com>
2021-10-22 23:06:37 -0300

    adding old classes as models

---
+++ src/main/java/model/Facturable.java
@@ -0,0 +1,19 @@
+package model;
+
+public interface Facturable {
+
+	 double obtenerCostoTotal();
+
+	 double obtenerTiempoTotal();
+
+	 boolean hayCupo();
+
+	 void restarCupo();
+
+	 boolean esPromocion();
+
+	 TipoDeAtraccion getTipo();
+
+	 boolean seEncuentraEnElFacturable(Facturable facturable);
+
+}
---
+++ src/main/java/model/PromoAxB.java
@@ -0,0 +1,45 @@
+package model;
+
+import java.util.List;
+
+public class PromoAxB extends Promocion {
+
+	public Facturable atraccionExtra;
+
+	public PromoAxB(List<Facturable> listaDeAtracciones, TipoDeAtraccion tipoDePromocion
+			, String nombreDePromocion, Facturable atraccionExtra) {
+		super(listaDeAtracciones, tipoDePromocion, nombreDePromocion);
+		this.atraccionExtra = atraccionExtra;
+	}
+
+	@Override
+	public double obtenerTiempoTotal() {
+		double tiempoTotal = super.obtenerTiempoTotal();
+		tiempoTotal += atraccionExtra.obtenerTiempoTotal();
+		return tiempoTotal;
+	}
+
+	@Override
+	public boolean hayCupo() {
+		boolean hayCupo = super.hayCupo();
+		return hayCupo && atraccionExtra.hayCupo();
+	}
+
+	@Override
+	public void restarCupo() {
+		super.restarCupo();
+		atraccionExtra.restarCupo();
+	}
+
+	@Override
+	public boolean seEncuentraEnElFacturable(Facturable facturable) {
+
+		return super.seEncuentraEnElFacturable(facturable) || this.atraccionExtra.seEncuentraEnElFacturable(facturable);
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() +" Atraccion de regalo: \n" + atraccionExtra;
+	}
+
+}
