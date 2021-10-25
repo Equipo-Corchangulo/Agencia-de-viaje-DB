9df8a1f007ee3ba60906bdc60869abb83f22f137
irinachurko <87023017+irinachurko@users.noreply.github.com>
2021-10-25 17:52:56 -0300

    Se crea UsuariosDAO

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,7 +1,9 @@
 package app;
 
 import dao.AtraccionesDAO;
+import dao.UsuariosDAO;
 import model.Atraccion;
+import model.PerfilUsuario;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -11,10 +13,22 @@ public class App {
 	public static void main(String[] args) throws SQLException {
 		// TODO Auto-generated method stub
 		//dbc:sqlite:C:\Users\josel\eclipse-workspace\conexion_db_agenciadeturismo\tierramedia.db
+		System.out.println("ATRACCIONES:");
+		
 		List<Atraccion> lista_atracciones = AtraccionesDAO.findAll();
 		for(Atraccion a : lista_atracciones){
 			System.out.println(a);
 		}
-	}
+		
+		System.out.println("--------------------");
+		System.out.println("USUARIOS:");
+		
+		List<PerfilUsuario> lista_usuarios = UsuariosDAO.findAll();
+		for(PerfilUsuario u : lista_usuarios){
+			System.out.println(u);
+		}
+		
+		
 
+	}
 }
